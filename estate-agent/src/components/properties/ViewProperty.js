ef560b05bcfa4b000a635e8adeb7bcdef3e20203
AnthonyP0404 <anthony.peterson00@gmail.com>
2023-12-07 17:01:33 +0000

    ready to present

--- estate-agent/src/components/properties/ViewProperty.js
+++ estate-agent/src/components/properties/ViewProperty.js
@@ -1,21 +1,35 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 
+import Carousel from 'react-bootstrap/Carousel';
+
+import imageApartment1   from './../../img/houses/APARTMENT/img1.jpg';
+import imageApartment2   from './../../img/houses/APARTMENT/img2.jpg';
+import imageApartment3   from './../../img/houses/APARTMENT/img3.jpg';
+
+import imageSemi1   from './../../img/houses/SEMI/img1.jpg';
+import imageSemi2   from './../../img/houses/SEMI/img2.jpg';
+import imageSemi3   from './../../img/houses/SEMI/img3.jpg';
+
+import imageDetached1   from './../../img/houses/DETACHED/img1.jpg';
+import imageDetached2   from './../../img/houses/DETACHED/img2.jpg';
+import imageDetached3   from './../../img/houses/DETACHED/img3.jpg';
+
 function ViewProperty() {
     let property = useLocation().state.property
     let navigate = useNavigate()
 
+
+    
+    let imageUrl1 = './../../img/houses/' +property.type+ '/img1.jpg'
+
     function statusCheck() {
-        console.log(property.status)
         switch (property.status) {
             case "FOR SALE":
-                console.log("for sale")
                 return createForSaleButtons()
             case "WITHDRAWN":
-                console.log("withdrawn")
                 return createWithdrawnButtons()
             default:
-                console.log("sold")
                 return createSoldButtons()
         }
     }
@@ -134,6 +148,11 @@ function ViewProperty() {
         }
     }
 
+
+    function chooseImage(num) {
+        return 
+    }
+
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     return (
@@ -142,19 +161,46 @@ function ViewProperty() {
                 <h1 id="pageHeading"> <b>Property #{property.id} </b> </h1>
                 <p style={{ color: 'white' }}> Below are all details surrounding the selected property. </p>
 
-                <table id="viewPropertyTable">
-                    <tr> <td style={{ width: "30%" }}> ID </td> <td> {property.id} </td> </tr>
-                    <tr> <td> Address   </td> <td> {property.address}   </td> </tr>
-                    <tr> <td> Postcode  </td> <td> {property.postcode}  </td> </tr>
-                    <tr> <td> Type      </td> <td> {property.type}      </td> </tr>
-                    <tr> <td> Price     </td> <td> {property.price}     </td> </tr>
-                    <tr> <td> Bedrooms  </td> <td> {property.bedroom}   </td> </tr>
-                    <tr> <td> Bathrooms </td> <td> {property.bathroom}  </td> </tr>
-                    <tr> <td> Gardens   </td> <td> {property.garden}    </td> </tr>
-                    <tr> <td> Seller ID </td> <td> {property.sellerId}  </td> </tr>
-                    <tr> <td> Status    </td> <td> {property.status}    </td> </tr>
-                    <tr> <td> Buyer ID  </td> <td> {property.buyerId}   </td> </tr>
-                </table> <br /><br />
+                <div className="container p-5">
+                    <div className="grid">
+                        <div className="row">
+                            <div className="col-md-6">
+                                <Carousel className="m-4">
+                                    <Carousel.Item>
+                                        {/* <img src={chooseImage(1)} /> */}
+                                        <img src={imageSemi1} />
+                                    </Carousel.Item>
+                                    <Carousel.Item>
+                                        {/* <img src={chooseImage(2)} /> */}
+                                        <img src={imageSemi2} />
+                                    </Carousel.Item>
+                                    <Carousel.Item>
+                                        {/* <img src={chooseImage(3)} /> */}
+                                        <img src={imageSemi3} />
+                                    </Carousel.Item>
+                                </Carousel>
+                            </div>
+
+                            <div className="col-md-6">
+                                <table id="viewPropertyTable">
+                                    <tr> <td style={{ width: "30%" }}> ID </td> <td> {property.id} </td> </tr>
+                                    <tr> <td> Address   </td> <td> {property.address}   </td> </tr>
+                                    <tr> <td> Postcode  </td> <td> {property.postcode}  </td> </tr>
+                                    <tr> <td> Type      </td> <td> {property.type}      </td> </tr>
+                                    <tr> <td> Price     </td> <td> {property.price}     </td> </tr>
+                                    <tr> <td> Bedrooms  </td> <td> {property.bedroom}   </td> </tr>
+                                    <tr> <td> Bathrooms </td> <td> {property.bathroom}  </td> </tr>
+                                    <tr> <td> Gardens   </td> <td> {property.garden}    </td> </tr>
+                                    <tr> <td> Seller ID </td> <td> {property.sellerId}  </td> </tr>
+                                    <tr> <td> Status    </td> <td> {property.status}    </td> </tr>
+                                    <tr> <td> Buyer ID  </td> <td> {property.buyerId}   </td> </tr>
+                                </table> <br /><br />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
 
                 <div id="viewPropertyButtons">
                     {statusCheck()}

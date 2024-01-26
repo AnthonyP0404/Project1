c1d688044e892bc85b6fe41dff30c6ff97d3a451
LyamWalburn <lyam567@hotmail.co.uk>
2024-01-26 16:54:46 +0000

    futher along broken nit test

--- estate-agent/src/components/bookings/booking.test.js
+++ estate-agent/src/components/bookings/booking.test.js
@@ -1,9 +1,9 @@
 //import { getBookingDateTimeString } from "../properties/ViewProperty"
 import ViewProperty from "../properties/ViewProperty"
 import {render,screen,cleanup} from '@testing-library/react'
-
- describe("make new booking", ()=> {
-
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import * as router from 'react-router-dom'
+import { Router } from "react-router-dom";
     let container = null;
 
     beforeEach(() => {
@@ -33,15 +33,50 @@ import {render,screen,cleanup} from '@testing-library/react'
     //  //expect(result).toEqual(expectedResult)
     // })
 
-    test("should render viewproperty component",()=>{
-        render(<ViewProperty/>)
-        const viewPropertyElement = screen.getAllByTestId('viewproperty-1');
-        expect(viewPropertyElement).toBeInTheDocument();
+
+    const mockUseLocationValue = {
+        pathname: 'http://localhost:3000/propertiesPage/viewProperty',
+        search:'',
+        state: { property:{
+            "id": 2,
+            "address": "22 Maple Street, Maple City",
+            "postcode": "MC1 1MC",
+            "type": "SEMI",
+            "price": 150000,
+            "bedroom": "5",
+            "bathroom": 2,
+            "garden": 1,
+            "sellerId": 2,
+            "status": "FOR SALE"
+          }},
+          hash:''
+    }
+    jest.mock('react-router-dom',()=>({
+        ...jest.requireActual('react-router-dom'),
+        useLocation: jest.fn().mockImplementation(() => {
+            return mockUseLocationValue;
+        })
+
+    }))
+    describe("<ViewProperty />",()=>{
+        it("should render ViewProperty",()=>{
+            render(<BrowserRouter><Routes><Route path="/propertiesPage/viewProperty" element={<ViewProperty/>} /></Routes></BrowserRouter>)
+            const viewPropertyElement = screen.getByTestId("viewproperty-1")
+            expect(viewPropertyElement).toBeInTheDocument();
+        })
     })
 
 
+    // test("should render viewproperty component",()=>{
+       
 
-})
+    //   //  jest.spyOn()
+    //     render(<ViewProperty/>)
+    //     const viewPropertyElement = screen.getByTestId("viewproperty-1")
+    //     expect(viewPropertyElement).toBeInTheDocument();
+    // })
+
+
 
 // describe('Addition', ()=>{
 //     it('knows that 2+2 = 4', ()=>{
